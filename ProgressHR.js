1d16addc76bb1a84b7d3cbd74d7cc2bb7697da5c
NiloCK <kolinkennedy@gmail.com>
2015-02-28 13:19:18 -0330

    html file to practice on. early method definitions

--- ProgressHR.js
+++ ProgressHR.js
@@ -1,5 +1,14 @@
 ﻿
 function ProgressHR() {
-    var sections = document.getElementsByClassName("ProgressHR");
+    var doc = document;
+    var progressItems = document.getElementsByClassName("ProgressHR");
+    console.log("There are " + progressItems.length + " ProgressItem(s)");
 
+    for (var i = 0; i < progressItems.length; i++) {
+
+        var sections = progressItems[i].innerHTML.split("<hr>"); // this is extremely bad
+        for (var j = 0; j < sections.length; j++) {
+            console.log(sections[j]);
+        }
+    }
 };
