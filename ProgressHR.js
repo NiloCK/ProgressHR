0c984cd9df29d5942d7b71af2d72bbe917255027
NiloCK <kolinkennedy@gmail.com>
2015-03-24 21:36:43 -0230

    Condensed things a little. Got rid of unneeded PHR class.

--- ProgressHR.js
+++ ProgressHR.js
@@ -1,64 +1,52 @@
-﻿/**
- * @constructor
- * @param {HTMLelement} hr - the HR to be converted
- * @param {Number} percent - the percentage to be represented
- */
-function PHR(hr, percent) {
+﻿function ProgressHR(options) {
+    
+    // default implementation defines how the HRs will be styled in
+    // absense of any user input.
+    var defaults = {
+        width: 80,      // 80% width
+        bgColor: null,  // null colors result in 'auto coloring'
+        barColor: null,
+        height: 5,
+        innerHeight: 2,
+        fcn: function (hr, percent) {
+            var outer = document.createElement('div');
+            var inner = document.createElement('div');
 
-    this.hr = hr;
-    this.percent = percent;
-
-    this.progress = function (options) {
-        this.options = {};
-
-        for (var key in this.defaults) {
-            if (this.defaults.hasOwnProperty(key)) {
-                this.options[key] = options[key] || this.defaults[key];
+            if (this.bgColor === null ||
+                this.barColor === null) {
+                //todo detect/set colors
             }
+            else {
+                outer.style.backgroundColor = this.bgColor;
+                inner.style.backgroundColor = this.barColor;
+            }
+
+            outer.style.width = this.width + '%';
+            outer.style.borderRadius = '5px';
+            outer.style.padding = '2px';
+
+            inner.style.width = '' + percent + '%';
+            inner.style.height = this.innerHeight + 'px';
+            inner.style.borderRadius = (this.innerHeight / 2) + 'px';
+
+            outer.appendChild(inner);
+            hr.parentNode.replaceChild(outer, hr);
         }
-
-        var outer = document.createElement('div');
-        var inner = document.createElement('div');
-
-        if (this.options.bgColor === null ||
-            this.options.barColor === null) {
-            //todo detect/set colors
-        }
-        else {
-            outer.style.backgroundColor = this.options.bgColor;
-            inner.style.backgroundColor = this.options.barColor;
-        }
-        
-        outer.style.width = this.options.width + '%';
-        outer.style.borderRadius = '5px';
-        outer.style.padding = '2px';
-
-        inner.style.width = '' + percent + '%';
-        inner.style.height = this.options.innerHeight + 'px';
-        inner.style.borderRadius = (this.options.innerHeight / 2) + 'px';
-        
-
-        outer.appendChild(inner);
-
-        hr.parentNode.replaceChild(outer, hr);
     };
-}
 
-PHR.prototype.defaults = {
-    width: 80,      // 80% width
-    bgColor: null,  // null colors result in 'auto coloring'
-    barColor: null,
-    height: 5,
-    innerHeight: 2
-};
-
-function ProgressHR(options) {
+    // apply those options supplied by the user to the 'default' object
+    // so that they will be used.
+    for (var key in options) {
+        if (options.hasOwnProperty(key) && defaults.hasOwnProperty(key)) {
+            defaults[key] = options[key];
+        }
+    }
 
     var progressItems = document.getElementsByClassName("ProgressHR");
     console.log("There are " + progressItems.length + " ProgressItem(s)");
     var hrs = document.getElementsByTagName('hr');
     console.log("There are " + hrs.length + " HR tags.");
-
+    
     for (var i = 0; i < progressItems.length; i++) {
         // todo: factor this out to a fcn ProgHRArticle(article){}
         var article = progressItems[i];
@@ -98,7 +86,10 @@ function ProgressHR(options) {
         // a (bad) example of 'the idea'
         for (var j = 0; j < itemHrs.length; j++) {
             //itemHrs[j].style.width = "" + Math.floor(percentages[j]) + "%";
-            new PHR(itemHrs[j], percentages[j]).progress(options || false);
+            //new PHR(itemHrs[j], percentages[j]).progress(options || false);
+            //phr(itemHrs[j], percentages[j]);
+            defaults.fcn(itemHrs[j], percentages[j]);
         }
-    }
+    }        
 };
+
